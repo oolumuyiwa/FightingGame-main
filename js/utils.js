2dc4c3b218f2968004eded666dc7bd206b3522ac
oolumuyiwa <79334116+oolumuyiwa@users.noreply.github.com>
2022-08-04 22:30:56 -0400

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -14,11 +14,11 @@ function determineWinner({ player, enemy, timerId }) {
 	clearTimeout(timerId)
 	document.querySelector('#displayText').style.display = 'flex';
 	if (player.health === enemy.health) {
-			document.querySelector('#displayText').innerHTML = 'TIE';
+			document.querySelector('#displayText').innerHTML = 'TIE.Refresh Page To Replay';
 		} else if (player.health > enemy.health) {
-			document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
+			document.querySelector('#displayText').innerHTML = 'Player 1 Wins. Refresh Page To Replay';
 		} else if (player.health < enemy.health) {
-			document.querySelector('#displayText').innerHTML = 'Player 2 Wins';
+			document.querySelector('#displayText').innerHTML = 'Player 2 Wins. Refresh Page To Replay';
 		}
 }
 
